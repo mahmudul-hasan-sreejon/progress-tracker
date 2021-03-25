01825008df943e6a3cd583982375069189c978de
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2021-03-25 19:58:15 +0600

    Code cleanup

--- detail_report_page.php
+++ detail_report_page.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-     <!-- Latest compiled and minified CSS -->
+    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
 
