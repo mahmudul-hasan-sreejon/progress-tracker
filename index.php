f70e3d7a235dfc64bbbf2e0e5f9a27c79b223e5e
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2021-03-26 20:32:51 +0600

    Update index page

--- index.php
+++ index.php
@@ -2,19 +2,27 @@
 
 require_once("resources/config.php");
 
+$currentPage = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1, -4);
+
 require_once(TEMPLATES_PATH . "/header.php");
 
 echo '<br>';
 
-echo '<div class="container-fluid">
-    <div class="row">';
-        require_once(TEMPLATES_PATH . "/sideBar.php");
+echo '
+    <div class="container-fluid">
+        <div class="row">
+';
 
-        require_once(TEMPLATES_PATH . "/chart1.php");
+require_once(TEMPLATES_PATH . "/sideBar.php");
 
-        require_once(TEMPLATES_PATH . "/chart2.php");
-    echo '</div>
-</div>';
+require_once(TEMPLATES_PATH . "/chart1.php");
+
+require_once(TEMPLATES_PATH . "/chart2.php");
+
+echo '
+        </div>
+    </div>
+';
 
 require_once(TEMPLATES_PATH . "/footer.php");
 
