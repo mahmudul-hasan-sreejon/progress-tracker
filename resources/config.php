ae1dc8ad871416057c90c7a73990988831e7afc3
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2021-03-27 01:04:16 +0600

    Update config.php

--- resources/config.php
+++ resources/config.php
@@ -8,24 +8,12 @@ $config = array(
             "password" => "",
             "host" => "localhost"
         )
-    ),
-
-    "urls" => array(
-        "baseUrl" => "https://progress-tracker.com"
-    ),
-
-    "paths" => array(
-        "resources" => $_SERVER["DOCUMENT_ROOT"] . "/resources",
-        "images" => $_SERVER["DOCUMENT_ROOT"] . "/images",
-        "icons" => $_SERVER["DOCUMENT_ROOT"] . "/images/icons"
     )
 );
-
 
 defined("LIBRARY_PATH") or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
 defined("TEMPLATES_PATH") or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
 defined("SCRIPTS_PATH") or define("SCRIPTS_PATH", realpath(dirname(__FILE__) . '/scripts'));
-
 
 ini_set("error_reporting", "true");
 error_reporting(E_ALL|E_STRCT);
