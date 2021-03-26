05cb1d1a3dfd45e1510528101e1624451b809d22
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2021-03-26 20:45:24 +0600

    Restructure list page

--- list.php
+++ list.php
@@ -1,77 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Test Project</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<?php    
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-</head>
+require_once("resources/config.php");
 
-<body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">Test Project</a>
-            </div>
+$currentPage = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1, -4);
 
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Chart</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="report.php">Poject Report</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="details.php">Detail Report</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">List</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+require_once(TEMPLATES_PATH . "/header.php");
 
-    <br><br>
+echo '<br>';
 
+echo '
     <div class="container-fluid">
         <div class="table-responsive-sm text-center">
-            <table class="table table-bordered table-striped table-hover">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Activities</th>
-                        <th>Project Name</th>
-                        <th>Score</th>
-                    </tr>
-                </thead>
+';
 
-                <tbody>
-                    <?php
-                    require('conn.php');
+require_once(TEMPLATES_PATH . "/activityList.php");
 
-                    $query = "SELECT project.project_name, activity.activity_name, activity.score FROM project INNER JOIN activity ON project.project_id = activity.project_id ORDER BY project.project_name ASC";
-                    $result = mysqli_query($conn, $query);
-
-                    while($row = mysqli_fetch_array($result)) {
-                        echo '
-                        <tr>
-                            <td>'.$row["activity_name"].'</td>
-                            <td>'.$row["project_name"].'</td>
-                            <td>'.$row["score"].'</td>
-                        </tr>';
-                    }
-
-                    mysqli_close($conn);
-                    ?>
-                </tbody>
-            </table>
+echo '
         </div>
     </div>
+';
 
-    <!-- Latest compiled JavaScript and jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-</body>
-</html>
+require_once(TEMPLATES_PATH . "/footer.php");
+
+?>
---
+++ resources/templates/activityList.php
@@ -0,0 +1,31 @@
+            <table class="table table-bordered table-striped table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Activities</th>
+                        <th>Project Name</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php
+
+                    $conn = mysqli_connect($config["db"]["mysql"]["host"], $config["db"]["mysql"]["username"], $config["db"]["mysql"]["password"], $config["db"]["mysql"]["dbname"]) or die(mysqli_connect_error());
+
+                    $query = "SELECT project.project_name, activity.activity_name, activity.score FROM project INNER JOIN activity ON project.project_id = activity.project_id ORDER BY project.project_name ASC";
+                    $result = mysqli_query($conn, $query);
+
+                    while($row = mysqli_fetch_array($result)) {
+                        echo '
+                        <tr>
+                            <td>'.$row["activity_name"].'</td>
+                            <td>'.$row["project_name"].'</td>
+                            <td>'.$row["score"].'</td>
+                        </tr>';
+                    }
+
+                    mysqli_close($conn);
+
+                    ?>
+                </tbody>
+            </table>
