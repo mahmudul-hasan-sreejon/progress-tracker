38bd234eebc894f5135056debc53fc2f34806177
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2021-03-26 22:27:11 +0600

    Add charts script

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -4,73 +4,24 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 
     <?php
-
     if($currentPage === "index") {
         echo '
             <!-- Morris chart Lib -->
             <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
             <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
-
-            <!-- Custom scripts -->
-            <script>
-                var chart_1 = Morris.Bar({
-                    element : "chart_1",
-                    data: [' . $chart_1_data . '],
-                    xkey: ["project_name"],
-                    ykeys: ["projected_days", "actual_days"],
-                    labels: ["Projected Days", "Actual Days"],
-                    hideHover: "auto",
-                    stacked: false,
-                    resize: true,
-                    barColors: ["#1fbba6", "#f8aa33", "#4da74d", "#afd8f8", "#edc240", "#cb4b4b", "#9440ed"],
-                    barOpacity: 0.9,
-                    barGap: 0,
-                    barSizeRatio: 0.46,
-                    gridTextSize: 10,
-                    xLabelAngle: 30
-                });
-
-                chart_1.options.labels.forEach(function(label, i) {
-                    var legendItem = $("<span></span>").text(label).css("color", chart_1.options.barColors[i]);
-
-                    $("#legend_1").append(legendItem);
-                });
-
-                function fetch_data() {
-                    $.ajax({
-                        url: "resources/library/chart/select.php",
-                        method: "POST",
-                        success: function(data) {
-                            $("#live_data").html(data);
-                        }
-                    });
-                }
-
-                fetch_data();
-
-                function update(activity_name) {
-                    // alert(activity_name);
-
-                    $.ajax({
-                        url: "resources/library/chart/update.php",
-                        method: "POST",
-                        data: {
-                            activity_name: activity_name
-                        },
-                        dataType: "text",
-                        success: function(data) {
-                            $("#live_data").html(data);
-                        }
-                    });
-                }
-            </script>
         ';
+    }
+    ?>
+
+    <!-- Custom scripts -->
+    <?php
+    if($currentPage === "index") {
+        require_once(SCRIPTS_PATH . "/charts.php");
     }
 
     if($currentPage === "details") {
         require_once(SCRIPTS_PATH . "/dataGrid.php");
     }
-
     ?>
 
 </body>
