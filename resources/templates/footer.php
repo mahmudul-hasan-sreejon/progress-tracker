0feeaf6b6e0a1ea9ae90f4634188d1abc616f5a1
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2021-03-26 20:21:27 +0600

    Update header and footer

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -3,61 +3,71 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 
-    <!-- Morris chart Lib -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+    <?php
 
-    <script>
-        var chart_1 = Morris.Bar({
-            element : 'chart_1',
-            data: [<?php echo $chart_1_data; ?>],
-            xkey: ['project_name'],
-            ykeys: ['projected_days', 'actual_days'],
-            labels: ['Projected Days', 'Actual Days'],
-            hideHover: 'auto',
-            stacked: false,
-            resize: true,
-            barColors: ['#1fbba6', '#f8aa33', '#4da74d', '#afd8f8', '#edc240', '#cb4b4b', '#9440ed'],
-            barOpacity: 0.9,
-            barGap: 0,
-            barSizeRatio: 0.46,
-            gridTextSize: 10,
-            xLabelAngle: 30
-        });
+    if($currentPage === "index") {
+        echo '
+            <!-- Morris chart Lib -->
+            <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 
-        chart_1.options.labels.forEach(function(label, i) {
-            var legendItem = $('<span></span>').text(label).css('color', chart_1.options.barColors[i]);
+            <!-- Custom scripts -->
+            <script>
+                var chart_1 = Morris.Bar({
+                    element : "chart_1",
+                    data: [' . $chart_1_data . '],
+                    xkey: ["project_name"],
+                    ykeys: ["projected_days", "actual_days"],
+                    labels: ["Projected Days", "Actual Days"],
+                    hideHover: "auto",
+                    stacked: false,
+                    resize: true,
+                    barColors: ["#1fbba6", "#f8aa33", "#4da74d", "#afd8f8", "#edc240", "#cb4b4b", "#9440ed"],
+                    barOpacity: 0.9,
+                    barGap: 0,
+                    barSizeRatio: 0.46,
+                    gridTextSize: 10,
+                    xLabelAngle: 30
+                });
 
-            $('#legend_1').append(legendItem);
-        });
+                chart_1.options.labels.forEach(function(label, i) {
+                    var legendItem = $("<span></span>").text(label).css("color", chart_1.options.barColors[i]);
 
-        function fetch_data() {
-            $.ajax({
-                url: "resources/library/chart/select.php",
-                method: "POST",
-                success: function(data) {
-                    $('#live_data').html(data);
+                    $("#legend_1").append(legendItem);
+                });
+
+                function fetch_data() {
+                    $.ajax({
+                        url: "resources/library/chart/select.php",
+                        method: "POST",
+                        success: function(data) {
+                            $("#live_data").html(data);
+                        }
+                    });
                 }
-            });
-        }
 
-        fetch_data();
+                fetch_data();
 
-        function update(activity_name) {
-            // alert(activity_name);
+                function update(activity_name) {
+                    // alert(activity_name);
 
-            $.ajax({
-                url: "resources/library/chart/update.php",
-                method: "POST",
-                data: {
-                    activity_name: activity_name
-                },
-                dataType: "text",
-                success: function(data) {
-                    $('#live_data').html(data);
+                    $.ajax({
+                        url: "resources/library/chart/update.php",
+                        method: "POST",
+                        data: {
+                            activity_name: activity_name
+                        },
+                        dataType: "text",
+                        success: function(data) {
+                            $("#live_data").html(data);
+                        }
+                    });
                 }
-            });
-        }
-    </script>
+            </script>
+        ';
+    }
+
+    ?>
+
 </body>
 </html>
--- resources/templates/header.php
+++ resources/templates/header.php
@@ -2,17 +2,26 @@
 <html>
 <head>
     <title>Test Project</title>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
-    <!-- Morris Chart Style -->
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+    <?php
 
-    <!-- User Defined Stylesheets -->
-    <link rel="stylesheet" href="styles/style.css">
+    if($currentPage === "index") {
+        echo '
+            <!-- Morris Chart Style -->
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+
+            <!-- Custom styles -->
+            <link rel="stylesheet" href="styles/style.css">
+        ';
+    }
+
+    ?>
 </head>
 
 <body>
@@ -23,21 +32,17 @@
             </div>
 
             <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Chart</a>
+                <li class="nav-item <?php echo ($currentPage === 'index') ? 'active' : ''; ?>">
+                    <a class="nav-link" href="index.php">Charts</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?php echo ($currentPage === 'report') ? 'active' : ''; ?>">
                     <a class="nav-link" href="report.php">Poject Report</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?php echo ($currentPage === 'details') ? 'active' : ''; ?>">
                     <a class="nav-link" href="details.php">Detail Report</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?php echo ($currentPage === 'list') ? 'active' : ''; ?>">
                     <a class="nav-link" href="list.php">List</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#nai"><?php echo $config["db"]["mysql"]["dbname"]; ?></a>
                 </li>
             </ul>
         </div>
